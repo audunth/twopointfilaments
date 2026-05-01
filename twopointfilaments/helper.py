d7d3ecb53d2854340811f0203b2dce89f90c27a8
audun.theodorsen@uit.no <audun.theodorsen@uit.no>
2026-05-01 11:10:00 +0200

    made file for TBTPW class

---
+++ twopointfilaments/helper.py
@@ -0,0 +1,23 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+import matplotlib.pyplot as plt
+
+def plot_far_sol_profiles(TBTPW):
+    def f(x):
+        return (1-np.exp(-x/TBTPW.doL))/(1-np.exp(-1./TBTPW.doL))
+    def M2(x,T):
+        # T is T(x,F)/TdF
+        return (1-(1-T*f(x)**2)**0.5)/(1+(1-T*f(x)**2)**0.5)
+        
+    def diffq(x,T):
+        c = 2*TBTPW.xi*TBTPW.ndF / (7*TBTPW.TdF**2)
+        return c*T**(-2.5)*f(x) * (0.5*T*(5+M2(x,T)) - TBTPW.ghat*TBTPW.xi)
+
+    x = np.linspace(0,1,1000)[::-1]
+    res=solve_ivp(diffq, [1.,0.], [1.,], t_eval=x)
+    n = 2/(res.y[0,:]*(M2(x,res.y[0,:])+1))
+
+    plt.figure()
+    plt.plot(x, res.y[0,:], label='T/Td')
+    plt.plot(x, np.sqrt(M2(x,res.y[0,:])), label='M')
+    plt.plot(x,n,label='n/nd')
---
+++ twopointfilaments/twobox.py
@@ -0,0 +1,99 @@
+import numpy as np
+from scipy.integrate import solve_ivp
+from scipy.optimize import root_scalar, minimize
+
+class TBTPW:
+    """
+    Two-Box Two-Point model with Wall fluxes.
+
+    Two-box extention of the two-point model including near-SOL and far-SOL,
+    ballooning and fluxes to the walls.
+
+    Input parameters:
+        coll: Reference collisionality
+        what: Perpendicular to parallel transport time
+        gamma: Sheath heat coeffificient
+        tau: Ion to electron temperature ratio. 
+        sqmass: Square root of electron to total mass ratio. 
+                Default for deuterium.
+        doL: normalized parallel ballooning length.
+    """
+    def __init__(self,coll,what,gamma=7,tau=1,sqmass=0.023,doL=0.33):
+        tautmp = (1+tau)
+        
+        self.xi = 7*tautmp**1.5*sqmass*coll/(3.2*2)
+        self.what = what
+        self.ghat = gamma/tautmp
+        self.doL = doL
+
+        self._calculate_near_sol()
+
+        if what>0:
+            self._calculate_far_sol()
+        else:
+            self.ndN=np.nan
+            self.ndF=np.nan
+            self.TdN=np.nan
+            self.TdF=np.nan
+
+    def _calculate_near_sol(self):
+        # The three equations combined give for the upstream temperature:
+        def f(TuN):
+            z0 = 2.5*self.what*self.doL*self.xi*TuN
+            e0 = 1-np.exp(-1/self.doL)
+            e1 = 1-self.doL*e0
+            
+            return TuN**3.5-(2*(1-z0*e0)/(self.ghat*self.xi*TuN))**(7) - 1 + z0*e1
+
+        res = root_scalar(f,bracket=(1e-5,1e5),x0=1)
+        self.TuN = res.root
+        self.TdN = (2*(1-2.5*self.what*self.doL*self.xi*self.TuN*(1-np.exp(-1/self.doL)))/(self.ghat*self.xi*self.TuN))**(2) 
+        self.ndN = 0.5*self.TuN/self.TdN
+        self.nuN = 1.
+
+
+
+    def _calculate_far_sol(self, x0=1.1):
+        theta = self._optimize_TuF(x0).x[0]
+        self.ndF, self.TdF = self._ndF_TdF(theta)
+        self.TuF = theta*self.TdF
+        self.nuF = 2*self.ndF/theta
+
+    def _ndF_TdF(self,theta):
+        # Returns ndF and TdF for given theta=TuF/TdF
+
+        a = self.what*self.doL*(1-np.exp(-1./self.doL))
+        c = 2.5*a/(self.ghat*self.xi)
+        b = c*self.TuN
+
+        # Canclulate sqrt(TdF)
+        P = np.polynomial.Polynomial([-2*b/theta, -b/a, 2*c, 1.])
+        roots = P.roots()
+        good0 = np.isreal(roots)
+        good1 = roots[good0]>0
+
+        TdFsq = np.real(roots[good0][good1])
+        assert len(TdFsq)==1, 'Too many positive TdF roots'
+        TdF = TdFsq[0]**2
+        ndF = a/(TdFsq[0]+2*a/theta)
+
+        return ndF, TdF
+
+    def _optimize_TuF(self,x0):
+        def f(x):
+            return (1-np.exp(-x/self.doL))/(1-np.exp(-1./self.doL))
+        def M2(x,T):
+            # T is T(x,F)/TdF
+            return (1-(1-T*f(x)**2)**0.5)/(1+(1-T*f(x)**2)**0.5)
+            
+        def diffq(x,T,ndF,TdF):
+            c = 2*self.xi*ndF / (7*TdF**2)
+            return c*T**(-2.5)*f(x) * (0.5*T*(5+M2(x,T)) - self.ghat*self.xi)
+            
+        def fitfun(theta):
+            ndF, TdF = self._ndF_TdF(theta[0])
+            res=solve_ivp(diffq, [1.,0.], [1.,], args=(ndF, TdF))
+            assert res.success, 'Did not find a solution to the energy equation'
+            return np.abs(theta-res.y[0,-1])
+
+        return minimize(fitfun, x0, bounds=((1e-1,10.),))
