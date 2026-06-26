fixed normalization in far-SOL for two-box

twopointfilaments/helper.py
@@ -11,7 +11,7 @@ def plot_far_sol_profiles(TBTPW):
         
     def diffq(x,T):
         c = 2*TBTPW.xi*TBTPW.ndF / (7*TBTPW.TdF**2)
-        return c*T**(-2.5)*f(x) * (0.5*T*(5+M2(x,T)) - TBTPW.ghat*TBTPW.xi)
+        return c*T**(-2.5)*f(x) * (0.5*T*(5+M2(x,T)) - TBTPW.ghat)
 
     x = np.linspace(0,1,1000)[::-1]
     res=solve_ivp(diffq, [1.,0.], [1.,], t_eval=x)

twopointfilaments/twobox.py
@@ -84,7 +84,7 @@ class TBTPW:
         # Returns ndF and TdF for given theta=TuF/TdF
 
         a = self.what*self.doL*(1-np.exp(-1./self.doL))
-        c = 2.5*a/(self.ghat*self.xi)
+        c = 2.5*a/(self.ghat)
         b = c*self.TuN
 
         # Canclulate sqrt(TdF)
@@ -109,7 +109,7 @@ class TBTPW:
             
         def diffq(x,T,ndF,TdF):
             c = 2*self.xi*ndF / (7*TdF**2)
-            return c*T**(-2.5)*f(x) * (0.5*T*(5+M2(x,T)) - self.ghat*self.xi)
+            return c*T**(-2.5)*f(x) * (0.5*T*(5+M2(x,T)) - self.ghat)
             
         def fitfun(theta):
             ndF, TdF = self._ndF_TdF(theta[0])